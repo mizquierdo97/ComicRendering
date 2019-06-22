beb8052cc8b8a6d7bc138b100f3c56477d0c9435
mizquierdo97 <mizquierdo97@gmail.com>
2019-06-22 09:47:58 +0200

    Menu and polish

---
+++ TFG/Assets/Scripts/CameraSimulation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSimulation : MonoBehaviour {
+
+    public CPC_CameraPath path;
+    bool wantToRestart = false;
+    bool readyToStop = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(wantToRestart)
+        {
+            if (readyToStop)
+            {
+                path.PausePath();
+                wantToRestart = false;
+                readyToStop = false;
+            }
+            else
+                readyToStop = true;           
+            
+        }
+	}
+    public void PlaySim()
+    {
+        if(path.IsPlaying())
+            path.PausePath();
+        else
+        {
+            if(path.GetCurrentTimeInWaypoint() == 0.0f && path.GetCurrentWayPoint() == 0)
+                path.PlayPath(30.0f);
+            else
+                path.ResumePath();
+        }
+           
+    }
+
+    public void RestartSim()
+    {
+        //path.PausePath();
+        path.SetCurrentWayPoint(0);
+        path.SetCurrentTimeInWaypoint(0.0f);
+        //path.StopPath();
+        wantToRestart = true;
+    }
+}
---
+++ TFG/Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void LoadDemoScene()
+    {
+        SceneManager.LoadScene("Chapter2", LoadSceneMode.Single);
+    }
+    public void LoadGeometryScene()
+    {
+        SceneManager.LoadScene("WebGLScene", LoadSceneMode.Single);
+    }
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
